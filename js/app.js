e8ceda84cc752e0470817cf1b2f83869997b2bcb
KurniawanRizky <92316345+Kurniawanrzk@users.noreply.github.com>
2022-04-13 08:34:00 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,3 +26,22 @@ function timeToSet(timee,selector){
 var settime = document.getElementsByName(timee)[0];
 settime.value = selector.value;
 }
+
+ function cacheInput(e) {
+    localStorage.setItem(e.attributes["name"].value, e.value)
+}
+
+window.onload = function () {
+    let form = document.getElementById("Forms");
+    let inputs = form.children;
+    for (let i = 0; i < inputs.length; i++) {
+        let el = inputs[i];
+        if (el.tagName.toLowerCase() != "select" || el.attributes["name"].value != "test") {
+            continue
+        }   
+        let cachedVal = localStorage.getItem(el.attributes["name"].value)
+        if (cachedVal != null) {
+            el.value = cachedVal;
+        }
+    }
+}
