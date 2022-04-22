25903f49c87bf893d2ce4ae90095f52c634943c0
KurniawanRizky <92316345+Kurniawanrzk@users.noreply.github.com>
2022-04-22 15:06:42 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -27,21 +27,3 @@ var settime = document.getElementsByName(timee)[0];
 settime.value = selector.value;
 }
 
- function cacheInput(e) {
-    localStorage.setItem(e.attributes["name"].value, e.value)
-}
-
-window.onload = function () {
-    let form = document.getElementById("Forms");
-    let inputs = form.children;
-    for (let i = 0; i < inputs.length; i++) {
-        let el = inputs[i];
-        if (el.tagName.toLowerCase() != "select" || el.attributes["name"].value != "test") {
-            continue
-        }   
-        let cachedVal = localStorage.getItem(el.attributes["name"].value)
-        if (cachedVal != null) {
-            el.value = cachedVal;
-        }
-    }
-}
